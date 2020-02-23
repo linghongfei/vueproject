461dacde716014d2475cf0a8bad52d3b2b3f30aa
midai <midai@nuanaideMacBook-Pro.local>
2020-02-23 15:39:35 +0800

    storage封装：sessionStorage存储

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,17 @@
 </template>
 
 <script>
+import storage from './../storage';
+
 export default{
-    name:'index'
+    name:'index',
+    mounted(){
+        storage.setItem('a',1); //新增 a:1
+        storage.setItem('user',{'uaerName':'lhf','pass':123}); //新增 user:{..}
+        console.log(storage.getItem('user'));
+        console.log(storage.getItem('user','pass'));
+        storage.clear('a'); // 删除 a
+        storage.clear('pass','user'); //删除user下的pass
+    }
 }
 </script>
